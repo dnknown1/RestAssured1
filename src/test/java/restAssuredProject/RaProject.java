26ddc19c913691c0198b95b81dd638453661dfaa
Debalaya Bodhak <debalaya.bodhak@ibm.com>
2022-07-01 16:02:33 +0530

    project update: structural update

--- src/test/java/restAssuredProject/RaProject.java
+++ src/test/java/restAssuredProject/RaProject.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class RaProject {
@@ -20,6 +21,9 @@ public class RaProject {
 		return RestAssured.given();
 	}
 	
+	private void validateNlog(int sts, Response r){
+		r.then().statusCode(sts).log().all();
+	}
 	@SuppressWarnings("unchecked")
 	private String genUserData(String user, String fn, String ln, String em, String pwd, String ph, int st){
 		JSONObject j1 = useJson();
@@ -42,61 +46,53 @@ public class RaProject {
 	@Test(dataProvider="loginData", dependsOnMethods={"firstPOST"})
 	public void login(String u, String p){
 		System.out.println("LOG IN");
-		connect(BASE_URI)
-		.queryParam("username", u)
-		.queryParam("password", p)
-		.get(API_PATH+"login")
-		.then()
-		.statusCode(200)
-		.log().all();
+		
+		validateNlog(200,
+			connect(BASE_URI)
+			.queryParam("username", u)
+			.queryParam("password", p)
+			.get(API_PATH+"login")
+		);
 	}
 	
 	@Test(dependsOnMethods={"firstPOST"}, dataProvider="putData")
 	public void firstPUT(String u, String fn, String ln, String em, String pwd, String ph, int st){
 		System.out.println("PUT");
 
-		connect(BASE_URI)
-		.contentType(ContentType.JSON)
-		.body(genUserData(u, fn, ln, em, pwd, ph, st))
-		.when()
-		.put(API_PATH+u)
-		.then()
-		.statusCode(200)
-		.log().all();
+		validateNlog(200,
+			connect(BASE_URI)
+			.contentType(ContentType.JSON)
+			.body(genUserData(u, fn, ln, em, pwd, ph, st))
+			.when()
+			.put(API_PATH+u)
+		);
 	}
 
 	@Test(dependsOnMethods={"firstPUT"},dataProvider="deleteData")
 	public void firstDELETE(String id){
 		System.out.println("DELETE");
-		connect(BASE_URI)
-		.delete(API_PATH+id)
-		.then()
-		.statusCode(200)
-		.log().all();
+		validateNlog(200, connect(BASE_URI).delete(API_PATH+id));
 	}
 
 	@Test(dataProvider="deleteData", dependsOnMethods={"firstDELETE"})
 	public void anotherGET(String u){
 		System.out.println("GET");
-		connect(BASE_URI)
-		.get(API_PATH + u)
-		.then()
-		.statusCode(200)
-		.log().body();
+		validateNlog(200, connect(BASE_URI).get(API_PATH+u));
 	}
 
 	@Test(dataProvider="postData")
 	public void firstPOST(String u, String fn, String ln, String em, String pwd, String ph, int st){
-		connect(BASE_URI)
-		.contentType(ContentType.JSON)
-		.body(genUserData(u, fn, ln, em, pwd, ph, st))
-		.when()
-		.post(API_PATH)
-		.then()
-		.statusCode(200)
-		.log().all();
+		System.out.println("CREATE");
+
+		validateNlog(200,
+			connect(BASE_URI)
+			.contentType(ContentType.JSON)
+			.body(genUserData(u, fn, ln, em, pwd, ph, st))
+			.when()
+			.post(API_PATH)
+		);
 	}
-	
+
 	@DataProvider(name="postData")
 	public Object[][] providerPOST(){
 		Object[][] postData = {
